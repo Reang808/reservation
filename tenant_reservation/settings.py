11715b808252e5d0ba70d3644489dfb7591242dc
jess364 <rethu7718@gmail.com>
2025-09-22 04:39:08 +0900

    tetting

--- tenant_reservation/settings.py
+++ tenant_reservation/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = ['reang-reservation.jp', 'www.reang-reservation.jp', '163.44.123.195',]
+ALLOWED_HOSTS = ['reang-reserve.jp', 'www.reang-resere.jp', '163.44.123.195',]
 # '127.0.0.1:8000', '127.0.0.1', 'localhost'
 
 
